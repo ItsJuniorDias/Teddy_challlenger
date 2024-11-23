67619ee019bb0398bf0122d10dd86bb19836565d
AlexandreJr <juniordias_@live.com>
2024-11-23 00:07:58 -0300

    chore: add custom fonts

---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+import { Container, Title } from "./styles";
+
+export const HomeScreen = () => {
+  return (
+    <Container>
+      <Title>Olá, seja bem-vindo!</Title>
+    </Container>
+  );
+};
---
+++ src/screens/HomeScreen/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #ffffff;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  font-family: "InterBold";
+  font-size: 32px;
+  color: #000000;
+`;
